d3843fd3f30e049b454cd4b3bb34a81a0113901b
Maayana24 <maayan2412001@gmail.com>
2025-07-22 15:19:02 +0300

    ???

---
+++ TriviaServer/TriviaServer/DataBaseManager.cs
@@ -0,0 +1,108 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Npgsql;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
+
+namespace TriviaServer
+{
+    public class DatabaseManager
+    {
+        private static DatabaseManager instance = null;
+        private static readonly object padlock = new object();
+        private string _connectionString = "Host=aws-0-eu-central-1.pooler.supabase.com;Database=postgres;Username=postgres.bwnhhzedlhrnmqjhqplm;Password=1234;Port=5432;SSL Mode=Require;Trust Server Certificate=true";
+
+        DatabaseManager()
+        {
+
+        }
+
+        public static DatabaseManager Instance
+        {
+            get
+            {
+                lock (padlock)
+                {
+                    if (instance == null)
+                    {
+                        instance = new DatabaseManager();
+                    }
+                    return instance;
+                }
+            }
+        }
+
+
+        public async Task<Question> GetQuestion(int id)
+        {
+            try
+            {
+                Question question = new Question();
+                using var connection = new NpgsqlConnection(_connectionString);
+                await connection.OpenAsync();
+
+                // Query Players table
+                string query = "SELECT * FROM \"public\".\"Questions\" WHERE id = " + id;
+                using var command = new NpgsqlCommand(query, connection);
+                using var reader = await command.ExecuteReaderAsync();
+
+                if (reader.Read())
+                {
+                    question.QuestionText = reader.GetString(reader.GetOrdinal("question"));
+                    question.Answer1Text = reader.GetString(reader.GetOrdinal("answer1"));
+                    question.Answer2Text = reader.GetString(reader.GetOrdinal("answer2"));
+                    question.Answer3Text = reader.GetString(reader.GetOrdinal("answer3"));
+                    question.Answer4Text = reader.GetString(reader.GetOrdinal("answer4"));
+                    question.CorrectAnswer = reader.GetInt16(reader.GetOrdinal("correctAnswer"));
+                }
+                return question;
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+
+        public async Task<List<Question>> GetQuestions()
+        {
+            try
+            {
+                List<Question> questionList = new List<Question>();
+                using var connection = new NpgsqlConnection(_connectionString);
+                await connection.OpenAsync();
+
+                // Query Players table
+                string query = "SELECT * FROM \"public\".\"Questions\"";
+                using var command = new NpgsqlCommand(query, connection);
+                using var reader = await command.ExecuteReaderAsync();
+
+                while (reader.Read())
+                {
+                    Question question = new Question();
+                    question.QuestionText = reader.GetString(reader.GetOrdinal("question"));
+                    question.Answer1Text = reader.GetString(reader.GetOrdinal("answer1"));
+                    question.Answer2Text = reader.GetString(reader.GetOrdinal("answer2"));
+                    question.Answer3Text = reader.GetString(reader.GetOrdinal("answer3"));
+                    question.Answer4Text = reader.GetString(reader.GetOrdinal("answer4"));
+                    question.CorrectAnswer = reader.GetInt16(reader.GetOrdinal("correctAnswer"));
+                    questionList.Add(question);
+                }
+                return questionList;
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+
+        public async void AddPlayer(string name)
+        {
+            using var connection = new NpgsqlConnection(_connectionString);
+            await connection.OpenAsync();
+
+            string query = $"INSERT INTO \"public\".\"Players\" (name) VALUES ('{name}')";
+            using var command = new NpgsqlCommand(query, connection);
+            var result = await command.ExecuteNonQueryAsync();
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ TriviaServer/TriviaServer/PlayerInformation.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TriviaServer
+{
+    public class PlayerInformation
+    {
+        public PlayerInformation(string Name)
+        {
+            this.Name = Name;
+        }
+
+        public string Name { get; set; }
+        public int Score { get; set; }
+        public float ThinkingTime { get; set; }
+        public int CurrentQuestion { get; set; }
+        public bool WaitingForProgress { get; set; }
+        public bool Active { get; set; }
+    }
+}
